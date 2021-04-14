72a620ea431db329cad86ad526bc5a6f85b6cec8
EvelinaZakharchenko <evelina26022000@gmail.com>
2021-04-14 23:06:26 +0300

    task 3 updated with using sort and 'set()'

--- task3.py
+++ task3.py
@@ -12,30 +12,42 @@ def create_array():
     original_array = [randint(-1000, 1000) for i in range(N)]
     return original_array
 
+# def find_max1(array):
+#     max1 = -1001
+#     for i in array:
+#         if i > max1:
+#             max1 = i
+#     return max1
+#
+#
+# def find_max2(array, max1):
+#     max2 = -1001
+#     for i in array:
+#         if max2 < i < max1:
+#             max2 = i
+#     if max2 == -1001:  # for case, when all numbers equal - will return two equal numbers, not clear requirements
+#         max2 = max1
+#     return max2
 
-def find_max1(array):
-    max1 = -1001
-    for i in array:
-        if i > max1:
-            max1 = i
-    return max1
+def sort_array(array):
+    array_to_set = set(array)
+    array_to_list = list(array_to_set)
+    array_to_list.sort()
+    return array_to_list
 
-
-def find_max2(array, max1):
-    max2 = -1001
-    for i in array:
-        if max2 < i < max1:
-            max2 = i
-    if max2 == -1001:  # for case, when all numbers equal - will return two equal numbers, not clear requirements
-        max2 = max1
-    return max2
 
 
 if __name__ == "__main__":
     print("Начальный массив: ")
     first_array = create_array()
     print(first_array)
-    max1 = find_max1(first_array)
-    max2 = find_max2(first_array, max1)
-    print("Результат = ", max1, ", ", max2, sep='')
+    sorted_array = sort_array(first_array)
+    if len(sorted_array) == 1:
+        print("Массив состоит из одинаковых элементов. Сответственно максимальный только один: ", first_array[1])
+        exit()
+    max1 = sorted_array[-1]
+    max2 = sorted_array[-2]
+    # max1 = find_max1(first_array)
+    # max2 = find_max2(first_array, max1)
+    print("Результат = ", max2, ", ", max1, sep='')
 
